eb8e1dbf77e580b8a8a6f2cb2170b398d0edefc6
ivonildo lima <ivonildodev@gmail.com>
2024-09-03 17:40:11 -0300

    site feito junto do curso fabrica de app

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,48 @@
+
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import './favoritos.css'
+import { Link } from 'react-router-dom';
+
+function Favoritos() {
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("@primeflix");
+        setFilmes(JSON.parse(minhaLista) || [])
+    }, [])
+
+    function excluirFilme(id) {
+        let filtroFilmes = filmes.filter((item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+        toast.success("Filme Removido com sucesso ")
+    }
+    return (
+        <div className="meus-filmes">
+            <h1>Meus Filmes</h1>
+            {filmes.length === 0 && <span>Voçê não possui nenhum Filme salvo :( </span>}
+
+            <ul>
+                {filmes.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+                            <div>
+                                <Link to={`/filme/${item.id}`}>Ver Detalhes</Link>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+
+export default Favoritos;
